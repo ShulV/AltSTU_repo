7789473b47e819b1e82f21e2fb3678ca7262d330
Victor Shulpov <vshulpov@list.ru>
2022-01-23 16:31:54 +0700

    auto commit from notebook

--- push-pull-PC.bat
+++ push-pull-PC.bat
@@ -1,4 +1,4 @@
 
 cd C:\Program Files\Git\
-git-bash.exe -c 'cd "C:\Vitya\AltSTU_repo";git pull; git add *;git commit -m "commit text";git push -u;exec bash;'
+git-bash.exe -c 'cd "C:\Vitya\AltSTU_repo";git pull; git add *;git commit -m "auto commit from PC";git push -u;exec bash;'
 pause
