0f0113cb4a2dad58518129a7ef9ab3b0c1f1fb0f
Victor Desctop <vshulpov@gmail.com>
2022-01-23 16:02:01 +0700

    commit text

--- push-pull-PC.bat
+++ push-pull-PC.bat
@@ -1,6 +1,4 @@
 
 cd C:\Program Files\Git\
-git-bash.exe -c 'cd "C:\Vitya\AltSTU_repo";
-git pull; 
-exec bash'
+git-bash.exe -c 'cd "C:\Vitya\AltSTU_repo";git pull; git add *;git commit -m "commit text";git push -u;exec bash;'
 pause
