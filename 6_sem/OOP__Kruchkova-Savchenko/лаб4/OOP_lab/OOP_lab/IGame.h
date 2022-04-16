c2fbe8abfc1057801dc7264c02350a4eccf6ab29
Victor Desctop <vshulpov@gmail.com>
2022-04-16 20:49:18 +0700

    auto commit from PC (boot)

--- 6_sem/OOP__Kruchkova-Savchenko/лаб4/OOP_lab/OOP_lab/IGame.h
+++ 6_sem/OOP__Kruchkova-Savchenko/лаб4/OOP_lab/OOP_lab/IGame.h
@@ -3,8 +3,6 @@ class IGame
 {
 public:
     virtual void displayAnswerResultMessage(bool isAnswerTrue) = 0;
-    virtual void addExperience() = 0;
     virtual void goToNextLevel() = 0;
-    virtual void addAnsweredExercises() = 0;
 };
 
