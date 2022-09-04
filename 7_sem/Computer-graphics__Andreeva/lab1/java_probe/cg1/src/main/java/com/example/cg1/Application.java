3e797119a0978e19b4159f207d40fa550eed4c8d
Victor Shulpov (NB) <vshulpov@list.ru>
2022-09-05 00:35:43 +0700

    sem 7: CG-lab1 : refactoring

--- 7_sem/Computer-graphics__Andreeva/lab1/java_probe/cg1/src/main/java/com/example/cg1/Application.java
+++ 7_sem/Computer-graphics__Andreeva/lab1/java_probe/cg1/src/main/java/com/example/cg1/Application.java
@@ -1,22 +1,16 @@
 package com.example.cg1;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Lab 1");
         stage.setScene(scene);
