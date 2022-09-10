578dc64f662c0ded47719ad5add9395af2389a9a
Victor Shulpov (NB) <vshulpov@list.ru>
2022-09-11 04:02:30 +0700

    NB

--- 7_sem/Computer-graphics__Andreeva/lab1/java_probe/cg1/src/main/java/com/example/cg1/Controller.java
+++ 7_sem/Computer-graphics__Andreeva/lab1/java_probe/cg1/src/main/java/com/example/cg1/Controller.java
@@ -27,66 +27,207 @@ public class Controller {
     @FXML
     private Canvas canvasPlot;
 
-    private final Object3D object3D = new Object3D();
+    private final Object3D object3D = new Object3D(); //проволочная фигура
+    private GraphicsContext gc;
 
     public Controller() throws URISyntaxException {
         object3D.readLinesFromFile(""); //TODO заменить абсолютный путь
     }
 
-    private void drawLine(Line3D line) {
-        GraphicsContext gc = canvasPlot.getGraphicsContext2D();
+    //FMXL конструктор
+    @FXML
+    public void initialize() {
+        gc = canvasPlot.getGraphicsContext2D();
+        gc.translate(canvasPlot.getWidth() / 2, canvasPlot.getHeight() / 2);
+
+    }
+    //нарисовать линию
+    private void drawLine(GraphicsContext gc, double xStart, double yStart, double xEnd, double yEnd) {
+        gc.setLineWidth(1.0);
+        gc.setStroke(Color.BLUE);
+
+//        //отражение координат, т.к. направления осей координат на canvas в java нестандартное
+//        yStart *= -1;
+//        yEnd *= -1;
+
+        gc.strokeLine(xStart, yStart, xEnd, yEnd);
+
+    }
+
+    //нарисовать линию
+    private void drawLine(GraphicsContext gc, Line3D line) {
         gc.setLineWidth(1.0);
         gc.setStroke(Color.BLUE);
         double xStart = line.getStartPoint().getX();
         double yStart = line.getStartPoint().getY();
         double xEnd = line.getEndPoint().getX();
         double yEnd = line.getEndPoint().getY();
+
+//        //отражение координат, т.к. направления осей координат на canvas в java нестандартное
+//        yStart *= -1;
+//        yEnd *= -1;
+
         gc.strokeLine(xStart, yStart, xEnd, yEnd);
     }
-    public void clearCanvas() {
-        GraphicsContext gc = canvasPlot.getGraphicsContext2D();
-        gc.clearRect(0, 0, canvasPlot.getWidth(), canvasPlot.getHeight());
+
+    //очистить холст
+    public void clearCanvas(GraphicsContext gc) {
+
+        gc.clearRect(-canvasPlot.getWidth() / 2,
+                -canvasPlot.getHeight() / 2,
+                canvasPlot.getWidth(),
+                canvasPlot.getHeight());
     }
 
-    public void drawFigure() {
+    //отрисовать оси координат
+    public void drawAxes(GraphicsContext gc) {
+        gc.setLineWidth(2.0);
+        gc.setStroke(Color.GREEN);
+
+        gc.strokeLine(0, 0, 500, 0);// Ox
+        gc.strokeLine(0, 0, 0, -500);// Oy
+        gc.strokeLine(0, 0, -500, 500);// Oz
+
+    }
+
+    //отрисовать все элементы (очистка, оси, фигура)
+    public void drawAll() {
+        clearCanvas(gc);
+        drawAxes(gc);
+        drawFigure(gc);
+    }
+
+    //отрисовать проволочную фигуру
+    public void drawFigure(GraphicsContext gc) {
         for (Line3D line: object3D.getLineList()
              ) {
-            drawLine(line);
+            //проецирование
+//            a = edge.Item1.X - float.Parse((edge.Item1.Z * 0.5 * Math.Cos(Math.PI / 4)).ToString());
+//            b = -edge.Item1.Y + float.Parse((edge.Item1.Z * 0.5 * Math.Cos(Math.PI / 4)).ToString());
+//            c = edge.Item2.X - float.Parse((edge.Item2.Z * 0.5 * Math.Cos(Math.PI / 4)).ToString());
+//            d = -edge.Item2.Y + float.Parse((edge.Item2.Z * 0.5 * Math.Cos(Math.PI / 4)).ToString());
+            double startX = line.getStartPoint().getX() - line.getStartPoint().getZ() * 0.5 * Math.cos(Math.PI / 4);
+            double startY = -line.getStartPoint().getY() + line.getStartPoint().getZ() * 0.5 * Math.cos(Math.PI / 4);
+            double endX = line.getEndPoint().getX() - line.getEndPoint().getZ() * 0.5 * Math.cos(Math.PI / 4);
+            double endY = -line.getEndPoint().getY() + line.getEndPoint().getZ() * 0.5 * Math.cos(Math.PI / 4);
+
+            
+            drawLine(gc, startX, startY, endX, endY);
         }
     }
-    public void onBtnIncreaseXClick(ActionEvent actionEvent) throws URISyntaxException {
+
+    //обработчик кнопки смещения по oX (увеличение)
+    public void onBtnMovingIncreaseXClick(ActionEvent actionEvent) throws URISyntaxException {
         object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(10.0, 0.0, 0.0));
-        clearCanvas();
-        drawFigure();
+        drawAll();
     }
 
-    public void onBtnReduceXClick(ActionEvent actionEvent) throws URISyntaxException {
+    //обработчик кнопки смещения по oX (уменьшение)
+    public void onBtnMovingReduceXClick(ActionEvent actionEvent) throws URISyntaxException {
         object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(-10.0, 0.0, 0.0));
-        clearCanvas();
-        drawFigure();
+        drawAll();
     }
 
-    public void onBtnIncreaseYClick(ActionEvent actionEvent) throws URISyntaxException {
+    //обработчик кнопки смещения по oY (увеличение)
+    public void onBtnMovingIncreaseYClick(ActionEvent actionEvent) throws URISyntaxException {
         object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(0.0, 10.0, 0.0));
-        clearCanvas();
-        drawFigure();
+        drawAll();
     }
 
-    public void onBtnReduceYClick(ActionEvent actionEvent) throws URISyntaxException {
+    //обработчик кнопки смещения по oY (уменьшение)
+    public void onBtnMovingReduceYClick(ActionEvent actionEvent) throws URISyntaxException {
         object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(0.0, -10.0, 0.0));
-        clearCanvas();
-        drawFigure();
+        drawAll();
     }
 
-    public void onBtnIncreaseZClick(ActionEvent actionEvent) throws URISyntaxException {
-        object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(0.0, 00.0, -10.0));
-        clearCanvas();
-        drawFigure();
+    //обработчик кнопки смещения по oZ (увеличение)
+    public void onBtnMovingIncreaseZClick(ActionEvent actionEvent) throws URISyntaxException {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(0.0, 0.0, 10.0));
+        drawAll();
     }
 
-    public void onBtnReduceZClick(ActionEvent actionEvent) throws URISyntaxException {
+    //обработчик кнопки смещения по oZ (уменьшение)
+    public void onBtnMovingReduceZClick(ActionEvent actionEvent) throws URISyntaxException {
         object3D.doAffineTransformation(AffineTransformationMatrix3D.getMovingMatrix(0.0, 0.0, -10.0));
-        clearCanvas();
-        drawFigure();
+        drawAll();
     }
+
+    //обработчик кнопки поворота вокруг oX (увеличение)
+    public void onBtnRotationIncreaseXClick(ActionEvent actionEvent) {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getXRotationMatrix(Math.toRadians(10)));
+        drawAll();
+    }
+
+    //обработчик кнопки поворота вокруг oX (уменьшение)
+    public void onBtnRotationReduceXClick(ActionEvent actionEvent) {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getXRotationMatrix(Math.toRadians(-10)));
+        drawAll();
+    }
+
+    //обработчик кнопки поворота вокруг oY (увеличение)
+    public void onBtnRotationIncreaseYClick(ActionEvent actionEvent) {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getYRotationMatrix(Math.toRadians(10)));
+        drawAll();
+    }
+
+    //обработчик кнопки поворота вокруг oY (уменьшение)
+    public void onBtnRotationReduceYClick(ActionEvent actionEvent) {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getYRotationMatrix(Math.toRadians(-10)));
+        drawAll();
+    }
+
+    //обработчик кнопки поворота вокруг oZ (увеличение)
+    public void onBtnRotationIncreaseZClick(ActionEvent actionEvent) {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getZRotationMatrix(Math.toRadians(10)));
+        drawAll();
+    }
+
+    //обработчик кнопки поворота вокруг oZ (уменьшение)
+    public void onBtnRotationReduceZClick(ActionEvent actionEvent) {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getZRotationMatrix(Math.toRadians(-10)));
+        drawAll();
+    }
+
+    //обработчик кнопки масштабирования относительно oX (увеличение в 1.5 раза)
+    public void onBtnScalingIncreaseXClick(ActionEvent actionEvent) throws Exception {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getScalingMatrix(
+                (double) 3/2, 1, 1));
+        drawAll();
+    }
+
+    //обработчик кнопки масштабирования относительно oX (уменьшение в 1.5 раза)
+    public void onBtnScalingReduceXClick(ActionEvent actionEvent) throws Exception {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getScalingMatrix(
+                (double) 2/3, 1, 1));
+        drawAll();
+    }
+
+    //обработчик кнопки масштабирования относительно oY (увеличение в 1.5 раза)
+    public void onBtnScalingIncreaseYClick(ActionEvent actionEvent) throws Exception {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getScalingMatrix(
+                1, (double) 3/2, 1));
+        drawAll();
+    }
+
+    //обработчик кнопки масштабирования относительно oY (уменьшение в 1.5 раза)
+    public void onBtnScalingReduceYClick(ActionEvent actionEvent) throws Exception {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getScalingMatrix(
+                1, (double) 2/3, 1));
+        drawAll();
+    }
+
+    //обработчик кнопки масштабирования относительно oZ (увеличение в 1.5 раза)
+    public void onBtnScalingIncreaseZClick(ActionEvent actionEvent) throws Exception {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getScalingMatrix(
+                1, 1, (double) 3/2));
+        drawAll();
+    }
+
+    //обработчик кнопки масштабирования относительно oZ (уменьшение в 1.5 раза)
+    public void onBtnScalingReduceZClick(ActionEvent actionEvent) throws Exception {
+        object3D.doAffineTransformation(AffineTransformationMatrix3D.getScalingMatrix(
+                1, 1, (double) 2/3));
+        drawAll();
+    }
+
 }
--- 7_sem/Computer-graphics__Andreeva/lab1/java_probe/cg1/src/main/java/com/example/cg1/models/AffineTransformationMatrix3D.java
+++ 7_sem/Computer-graphics__Andreeva/lab1/java_probe/cg1/src/main/java/com/example/cg1/models/AffineTransformationMatrix3D.java
@@ -18,13 +18,62 @@ abstract public class AffineTransformationMatrix3D {
     //получить матрицу перемещения 4x4
     public static double[][] getMovingMatrix(double toX, double toY, double toZ) {
         double[][] matrix = getZeroMatrix();
-        matrix[0][0] = 1.0F;
-        matrix[1][1] = 1.0F;
-        matrix[2][2] = 1.0F;
+        matrix[0][0] = 1;
+        matrix[1][1] = 1;
+        matrix[2][2] = 1;
         matrix[3][0] = toX;
         matrix[3][1] = toY;
         matrix[3][2] = toZ;
-        matrix[3][3] = 1.0F;
+        matrix[3][3] = 1;
+        return matrix;
+    }
+
+    //получить матрицу поворота 4x4 вокруг оси X
+    public static double[][] getXRotationMatrix(double angle) {
+        double[][] matrix = getZeroMatrix();
+        matrix[0][0] = 1;
+        matrix[1][1] = Math.cos(angle);
+        matrix[1][2] = Math.sin(angle);
+        matrix[2][1] = -Math.sin(angle);
+        matrix[2][2] = Math.cos(angle);
+        matrix[3][3] = 1;
+        return matrix;
+    }
+
+    //получить матрицу поворота 4x4 вокруг оси Y
+    public static double[][] getYRotationMatrix(double angle) {
+        double[][] matrix = getZeroMatrix();
+        matrix[0][0] = Math.cos(angle);
+        matrix[0][2] = -Math.sin(angle);
+        matrix[1][1] = 1;
+        matrix[2][0] = Math.sin(angle);
+        matrix[2][2] = Math.cos(angle);
+        matrix[3][3] = 1;
+        return matrix;
+    }
+
+    //получить матрицу поворота 4x4 вокруг оси Z
+    public static double[][] getZRotationMatrix(double angle) {
+        double[][] matrix = getZeroMatrix();
+        matrix[0][0] = Math.cos(angle);
+        matrix[0][1] = Math.sin(angle);
+        matrix[1][0] = -Math.sin(angle);
+        matrix[1][1] = Math.cos(angle);
+        matrix[2][2] = 1;
+        matrix[3][3] = 1;
+        return matrix;
+    }
+
+    //получить матрицу масштабирования 4x4
+    public static double[][] getScalingMatrix(double forX, double forY, double forZ) throws Exception {
+        if (forX <= 0 || forY <=0 || forZ <= 0) {
+            throw new Exception("scale factor cannot be less than zero");
+        }
+        double[][] matrix = getZeroMatrix();
+        matrix[0][0] = forX;
+        matrix[1][1] = forY;
+        matrix[2][2] = forZ;
+        matrix[3][3] = 1;
         return matrix;
     }
 }
