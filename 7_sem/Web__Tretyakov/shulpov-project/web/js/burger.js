a0614ac91bbc9d2b6feff296a5ef92b2593c3efc
Victor Shulpov (NB) <vshulpov@list.ru>
2022-10-20 15:29:41 +0700

    NB

--- 7_sem/Web__Tretyakov/shulpov-project/web/js/burger.js
+++
@@ -1,5 +0,0 @@
-let burger = document.querySelector('.header__nav-burger');
-
-burger.addEventListener('click', (e) => {
-  document.querySelector('.header__nav-list').classList.toggle('header__nav-list_active');
-})
